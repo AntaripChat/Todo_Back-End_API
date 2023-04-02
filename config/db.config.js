aff904cfd624dd6de1108dc52e8db1c562bc8d8a
Antarip Chatterjee <chatterjeeantarip1234@gmail.com>
2023-04-02 20:09:48 +0530

    add mongodb atlas link

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,7 @@
 const mongo = require('mongoose');
-
-mongo.connect('mongodb://127.0.0.1:27017/todo').then(()=>{
+//mongodb+srv://todo:<password>@cluster0.mgkhtei.mongodb.net/?retryWrites=true&w=majority
+//mongodb://127.0.0.1:27017/todo
+mongo.connect('mongodb+srv://todo:todo123@cluster0.mgkhtei.mongodb.net/?retryWrites=true&w=majority').then(()=>{
     console.log("DB Connect");
 }).catch((err)=>{
     console.log(err);
